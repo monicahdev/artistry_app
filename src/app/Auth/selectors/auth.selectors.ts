c8a68c97ead14b994c361dfa3a13bdb598b10064
Monica Hernandez <monicahdev@gmail.com>
2025-11-30 20:13:02 +0100

    Defined auth selectors
    AuthState, AuthError and AuthCredentials

---
+++ src/app/Auth/selectors/auth.selectors.ts
@@ -0,0 +1,19 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AuthState } from '../reducers';
+
+export const selectAuthState = createFeatureSelector<AuthState>('auth');
+
+export const selectAuthLoading = createSelector(
+  selectAuthState,
+  (state) => state.loading
+);
+
+export const selectAuthError = createSelector(
+  selectAuthState,
+  (state) => state.error
+);
+
+export const selectAuthCredentials = createSelector(
+  selectAuthState,
+  (state) => state.credentials
+);
