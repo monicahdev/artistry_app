a05002e57eae227e3284b4d5369d5882a0e83070
Monica Hernandez <monicahdev@gmail.com>
2025-11-30 21:26:00 +0100

    Added paths routes for the rest of the app components.
    For now, the ones with components yet to be created redirect to Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Auth/components/login/login.component';
 import { RegisterComponent } from './Auth/components/register/register.component';
+import { AboutMeComponent } from './Public/about-me/about-me.component';
 import { HomeComponent } from './Public/home/home.component';
+import { PortfolioComponent } from './Public/portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'makeup-services', component: HomeComponent },
+  { path: 'online-classes', component: HomeComponent },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
 ];
