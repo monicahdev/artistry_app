4efd49e08b7fe5ffb681d135b4a32a7774507e62
Monica Hernandez <monicahdev@gmail.com>
2025-11-30 20:29:09 +0100

    Added login and register paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Auth/components/login/login.component';
+import { RegisterComponent } from './Auth/components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
