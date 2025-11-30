8d773cb136b9281d5434518146086eacdfbb9c45
Monica Hernandez <monicahdev@gmail.com>
2025-11-30 20:11:43 +0100

    Imported auth reducer, and auth state

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,5 +1,10 @@
 import { ActionReducerMap } from '@ngrx/store';
+import { authReducer, AuthState } from './Auth/reducers/auth.reducers';
 
-export interface AppState {}
+export interface AppState {
+  auth: AuthState;
+}
 
-export const appReducers: ActionReducerMap<AppState> = {};
+export const appReducers: ActionReducerMap<AppState> = {
+  auth: authReducer,
+};
