49e0636a9859b30f905b50e1add4b26bfccae36d
Monica Hernandez <monicahdev@gmail.com>
2025-12-01 15:14:58 +0100

    Added User selectors

---
+++ src/app/User/selectors/user.selectors.ts
@@ -0,0 +1,19 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { UserState } from '../reducers/user.reducer';
+
+export const selectUserState = createFeatureSelector<UserState>('user');
+
+export const selectCurrentUser = createSelector(
+  selectUserState,
+  (state) => state.user
+);
+
+export const selectUserLoading = createSelector(
+  selectUserState,
+  (state) => state.loading
+);
+
+export const selectUserError = createSelector(
+  selectUserState,
+  (state) => state.error
+);
