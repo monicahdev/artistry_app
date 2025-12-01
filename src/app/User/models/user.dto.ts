d913cb1342a662c53c65e56dae96d423c8efff66
Monica Hernandez <monicahdev@gmail.com>
2025-12-01 11:01:40 +0100

    Created UseDTO and MeResponseDTO

---
+++ src/app/User/models/user.dto.ts
@@ -0,0 +1,31 @@
+export class UserDTO {
+  id: number;
+  email: string;
+  role: 'USER' | 'ADMIN';
+  created_at?: Date;
+  updated_at?: Date;
+
+  constructor(
+    id: number,
+    email: string,
+    role: 'USER' | 'ADMIN',
+    created_at?: string,
+    updated_at?: string
+  ) {
+    this.id = id;
+    this.email = email;
+    this.role = role;
+    this.created_at = created_at ? new Date(created_at) : undefined;
+    this.updated_at = updated_at ? new Date(created_at) : undefined;
+  }
+}
+
+export class MeResponseDTO {
+  message: string;
+  user: UserDTO;
+
+  constructor(message: string, user: UserDTO) {
+    this.message = message;
+    this.user = user;
+  }
+}
