a790807b922525e149eb9f09be8acb011c5969a4
Monica Hernandez <monicahdev@gmail.com>
2025-12-01 09:04:39 +0100

    Created auth interceptor

--- src/app/Shared/Services/auth-interceptor.service.ts
+++ src/app/Shared/Services/auth-interceptor.service.ts
@@ -1,9 +1,28 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthInterceptorService {
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('access_token');
 
-  constructor() { }
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+
+    return next.handle(request);
+  }
 }
