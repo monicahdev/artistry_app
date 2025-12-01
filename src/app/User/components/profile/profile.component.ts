a5b3c468c52ea32809ba93b268768921234fe77f
Monica Hernandez <monicahdev@gmail.com>
2025-12-01 19:21:47 +0100

    Added selectCurrentUser selector to Profile component constructor

--- src/app/User/components/profile/profile.component.ts
+++ src/app/User/components/profile/profile.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../../app.reducers';
+import { UserDTO } from '../../models/user.dto';
+import { selectCurrentUser } from '../../selectors/user.selectors';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  user$: Observable<UserDTO | null>;
 
+  constructor(private store: Store<AppState>) {
+    this.user$ = this.store.select(selectCurrentUser);
+  }
 }
