ec4cf5832b06c76a086831b703b32ec3237860ee
Monica Hernandez <monicahdev@gmail.com>
2025-12-01 21:13:00 +0100

    Added auth guard to profile path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { RegisterComponent } from './Auth/components/register/register.component
 import { AboutMeComponent } from './Public/about-me/about-me.component';
 import { HomeComponent } from './Public/home/home.component';
 import { PortfolioComponent } from './Public/portfolio/portfolio.component';
+import { AuthGuard } from './Shared/Guards/auth-guard.guard';
 import { ProfileComponent } from './User/components/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'about-me', component: AboutMeComponent },
   { path: 'portfolio', component: PortfolioComponent },
   { path: 'makeup-services', component: HomeComponent },
