3ad9fd2a1556be7d26026f611c739b4c8a79f2d6
Monica Hernandez <monicahdev@gmail.com>
2025-12-01 15:14:02 +0100

    Imported User reducers

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,10 +1,13 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { authReducer, AuthState } from './Auth/reducers/auth.reducers';
+import { userReducer, UserState } from './User/reducers/user.reducer';
 
 export interface AppState {
   auth: AuthState;
+  user: UserState;
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
   auth: authReducer,
+  user: userReducer,
 };
