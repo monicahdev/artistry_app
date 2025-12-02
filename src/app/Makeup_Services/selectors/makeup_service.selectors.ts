d271dc8ea0314052e5c28894a2ca2c58b01cd5f4
Monica Hernandez <monicahdev@gmail.com>
2025-12-02 12:00:20 +0100

    Created makeup_services selectors
    Included selectall makeupservices, loading and error selectors

---
+++ src/app/Makeup_Services/selectors/makeup_service.selectors.ts
@@ -0,0 +1,20 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { MakeupServicesState } from '../reducers/makeup_services.reducers';
+
+export const selectMakeupServicesState =
+  createFeatureSelector<MakeupServicesState>('makeupServices');
+
+export const selectAllMakeupServices = createSelector(
+  selectMakeupServicesState,
+  (state) => state.services
+);
+
+export const selectMakeupServicesLoading = createSelector(
+  selectMakeupServicesState,
+  (state) => state.loading
+);
+
+export const selectMakeupServicesError = createSelector(
+  selectMakeupServicesState,
+  (state) => state.error
+);
