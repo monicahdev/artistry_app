148d297b1252bf7964eb9549c7330201c97c477b
Monica Hernandez <monicahdev@gmail.com>
2025-12-02 12:01:18 +0100

    Added makeup_services reducer to general app reducers

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,13 +1,18 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { authReducer, AuthState } from './Auth/reducers/auth.reducers';
+import {
+  makeupServicesReducer,
+  MakeupServicesState,
+} from './Makeup_Services/reducers/makeup_services.reducers';
 import { userReducer, UserState } from './User/reducers/user.reducer';
-
 export interface AppState {
   auth: AuthState;
   user: UserState;
+  makeupServices: MakeupServicesState;
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
   auth: authReducer,
   user: userReducer,
+  makeupServices: makeupServicesReducer,
 };
