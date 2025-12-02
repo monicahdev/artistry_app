673d781ed9827bffacd607bf0ee16ada79d46933
Monica Hernandez <monicahdev@gmail.com>
2025-12-02 11:57:26 +0100

    Added makeup_services reducers

---
+++ src/app/Makeup_Services/reducers/makeup_services.reducers.ts
@@ -0,0 +1,50 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import * as MakeupServicesActions from '../actions/makeup_services.actions';
+import { MakeupServiceDTO } from '../models/makeup_service.dto';
+
+export interface MakeupServicesState {
+  services: MakeupServiceDTO[];
+  loading: boolean;
+  loaded: boolean;
+  error: any;
+}
+
+export const initialState: MakeupServicesState = {
+  services: [],
+  loading: false,
+  loaded: false,
+  error: null,
+};
+
+const _makeupServicesReducer = createReducer(
+  initialState,
+  on(MakeupServicesActions.loadMakeupServices, (state) => ({
+    ...state,
+    loading: true,
+    loaded: false,
+    error: null,
+  })),
+  on(
+    MakeupServicesActions.loadMakeupServicesSuccess,
+    (state, { services }) => ({
+      ...state,
+      services,
+      loading: false,
+      loaded: true,
+      error: null,
+    })
+  ),
+  on(MakeupServicesActions.loadMakeupServicesFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    loaded: false,
+    error,
+  }))
+);
+
+export function makeupServicesReducer(
+  state: MakeupServicesState | undefined,
+  action: Action
+): MakeupServicesState {
+  return _makeupServicesReducer(state, action);
+}
