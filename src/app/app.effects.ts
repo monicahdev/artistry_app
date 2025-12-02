940f2412d398c4a95dbc4d7c36a4bc74ceed8ba1
Monica Hernandez <monicahdev@gmail.com>
2025-12-02 11:44:33 +0100

    Added makeup services effects to app effects

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,3 +1,4 @@
 import { AuthEffects } from './Auth/effects/auth.effects';
+import { MakeupServicesEffects } from './Makeup_Services/effects/makeup_services.effects';
 import { UserEffects } from './User/effects/user.effects';
-export const appEffects = [AuthEffects, UserEffects];
+export const appEffects = [AuthEffects, UserEffects, MakeupServicesEffects];
