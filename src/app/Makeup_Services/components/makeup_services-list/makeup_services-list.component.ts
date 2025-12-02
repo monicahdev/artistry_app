194c4ad5f44c3e01099c9aef3e444120ff43a0cf
Monica Hernandez <monicahdev@gmail.com>
2025-12-02 12:27:51 +0100

    Added logic to makeup services list component
    When OnInit the loadServices dispatches. connects AppState selectors

--- src/app/Makeup_Services/components/makeup_services-list/makeup_services-list.component.ts
+++ src/app/Makeup_Services/components/makeup_services-list/makeup_services-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../../app.reducers';
+import * as MakeupServicesActions from '../../actions/makeup_services.actions';
+import { MakeupServiceDTO } from '../../models/makeup_service.dto';
+import {
+  selectAllMakeupServices,
+  selectMakeupServicesLoading,
+} from '../../selectors/makeup_service.selectors';
 @Component({
-  selector: 'app-services-list',
-  templateUrl: './services-list.component.html',
-  styleUrl: './services-list.component.scss'
+  selector: 'app-makeup_services-list',
+  templateUrl: './makeup_services-list.component.html',
+  styleUrls: ['./makeup_services-list.component.scss'],
 })
-export class ServicesListComponent {
+export class MakeupServicesListComponent implements OnInit {
+  services$: Observable<MakeupServiceDTO[]>;
+  loading$: Observable<boolean>;
 
+  constructor(private store: Store<AppState>) {
+    this.services$ = this.store.select(selectAllMakeupServices);
+    this.loading$ = this.store.select(selectMakeupServicesLoading);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(MakeupServicesActions.loadMakeupServices());
+  }
 }
