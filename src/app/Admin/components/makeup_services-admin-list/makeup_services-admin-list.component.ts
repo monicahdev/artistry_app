0947e109b1162f44d907a19d232bd8bf9d2aa3ea
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 14:54:57 +0100

    Added logic to load, create, and delete services
    Loads public makeup services, creates new makeup service in new route, confirms deletion of makeup service

--- src/app/Admin/components/makeup_services-admin-list/makeup_services-admin-list.component.ts
+++ src/app/Admin/components/makeup_services-admin-list/makeup_services-admin-list.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../../app.reducers';
+import * as MakeupServicesActions from '../../../Makeup_Services/actions/makeup_services.actions';
+import { MakeupServiceDTO } from '../../../Makeup_Services/models/makeup_service.dto';
+import {
+  selectAllMakeupServices,
+  selectMakeupServicesLoading,
+} from '../../../Makeup_Services/selectors/makeup_service.selectors';
+import * as AdminActions from '../../actions/admin.actions';
 
 @Component({
   selector: 'app-makeup_services-admin-list',
   templateUrl: './makeup_services-admin-list.component.html',
   styleUrls: ['./makeup_services-admin-list.component.scss'],
 })
-export class MakeupServicesAdminListComponent {}
+export class MakeupServicesAdminListComponent implements OnInit {
+  makeup_services$: Observable<MakeupServiceDTO[]>;
+  loading$: Observable<boolean>;
+
+  constructor(private store: Store<AppState>, private router: Router) {
+    this.makeup_services$ = this.store.select(selectAllMakeupServices);
+    this.loading$ = this.store.select(selectMakeupServicesLoading);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(MakeupServicesActions.loadMakeupServices());
+  }
+
+  onNewMakeupService(): void {
+    this.router.navigate(['/admin/services/new']);
+  }
+
+  onDeleteMakeupService(id: number): void {
+    const confirmed = confirm('¿Quieres eliminar este servicio?');
+    if (!confirmed) return;
+
+    this.store.dispatch(AdminActions.deleteAdminMakeupService({ id }));
+  }
+}
