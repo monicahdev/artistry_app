8ecc672dc8fff630362127533ff57fd15d62af5a
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 14:21:46 +0100

    Included admin effects to general app effects

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,4 +1,10 @@
+import { AdminEffects } from './Admin/effects/admin.effects';
 import { AuthEffects } from './Auth/effects/auth.effects';
 import { MakeupServicesEffects } from './Makeup_Services/effects/makeup_services.effects';
 import { UserEffects } from './User/effects/user.effects';
-export const appEffects = [AuthEffects, UserEffects, MakeupServicesEffects];
+export const appEffects = [
+  AuthEffects,
+  UserEffects,
+  MakeupServicesEffects,
+  AdminEffects,
+];
