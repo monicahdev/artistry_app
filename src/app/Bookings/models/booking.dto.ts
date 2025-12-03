56def9ba3ece3bc07a7d47226da61a074e456507
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 16:10:51 +0100

    Created bookingDTO and BookingCreateDTO

---
+++ src/app/Bookings/models/booking.dto.ts
@@ -0,0 +1,19 @@
+export class BookingDTO {
+  constructor(
+    public id: number,
+    public user_id: number,
+    public service_id: number,
+    public date_hour: string,
+    public comments: string | null,
+    public status: string,
+    public created_at: string
+  ) {}
+}
+
+export class BookingCreateDTO {
+  constructor(
+    public service_id: number,
+    public date_hour: string,
+    public comments?: string
+  ) {}
+}
