6441b3fe2543a7cb143d51037cc72b94967cf9c4
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 14:08:28 +0100

    Created admin reducers
    reducers for loading all makeupservices, and creating and deleting a makeup_service

---
+++ src/app/Admin/reducers/admin.reducers.ts
@@ -0,0 +1,88 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { MakeupServiceDTO } from '../../Makeup_Services/models/makeup_service.dto';
+import * as AdminActions from '../actions/admin.actions';
+
+export interface AdminState {
+  makeup_services: MakeupServiceDTO[];
+  loading: boolean;
+  loaded: boolean;
+  error: any;
+}
+
+export const initialState: AdminState = {
+  makeup_services: [],
+  loading: false,
+  loaded: false,
+  error: null,
+};
+
+const _adminReducer = createReducer(
+  initialState,
+
+  // Cargar servicios maquillaje
+  on(AdminActions.loadAdminMakeupServices, (state) => ({
+    ...state,
+    loading: true,
+    loaded: false,
+    error: null,
+  })),
+  on(
+    AdminActions.loadAdminMakeupServicesSuccess,
+    (state, { makeup_services }) => ({
+      ...state,
+      makeup_services,
+      loading: false,
+      loaded: true,
+      error: null,
+    })
+  ),
+  on(AdminActions.loadAdminMakeupServicesFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    loaded: false,
+    error,
+  })),
+
+  // Crear servicio de maquillaje
+  on(AdminActions.createAdminMakeupService, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(
+    AdminActions.createAdminMakeupServiceSuccess,
+    (state, { makeup_service }) => ({
+      ...state,
+      makeup_services: [...state.makeup_services, makeup_service],
+      loading: false,
+      loaded: true,
+    })
+  ),
+  on(AdminActions.createAdminMakeupServiceFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
+  })),
+
+  // Emilinar servicio maquillaje
+  on(AdminActions.deleteAdminMakeupService, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(AdminActions.deleteAdminMakeupServiceSuccess, (state, { id }) => ({
+    ...state,
+    makeup_services: state.makeup_services.filter((s) => s.id !== id),
+    loading: false,
+    loaded: true,
+  })),
+  on(AdminActions.deleteAdminMakeupServiceFailure, (state, { error }) => ({
+    ...state,
+    loading: false,
+    error,
+  }))
+);
+export function adminReducer(
+  state: AdminState | undefined,
+  action: Action
+): AdminState {
+  return _adminReducer(state, action);
+}
