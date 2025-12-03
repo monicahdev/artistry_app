3a47a68cc149de65aea89208e5285a7febefc338
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 14:21:28 +0100

    Included admin Reducer to general app reducers

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,18 +1,22 @@
 import { ActionReducerMap } from '@ngrx/store';
+import { adminReducer, AdminState } from './Admin/reducers';
 import { authReducer, AuthState } from './Auth/reducers/auth.reducers';
 import {
   makeupServicesReducer,
   MakeupServicesState,
 } from './Makeup_Services/reducers/makeup_services.reducers';
 import { userReducer, UserState } from './User/reducers/user.reducer';
+
 export interface AppState {
   auth: AuthState;
+  admin: AdminState;
   user: UserState;
   makeupServices: MakeupServicesState;
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
   auth: authReducer,
+  admin: adminReducer,
   user: userReducer,
   makeupServices: makeupServicesReducer,
 };
