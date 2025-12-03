8cc580dd656f399be578f656c1cdf8ed6174800d
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 16:07:07 +0100

    Created bookings list and form components

---
+++ src/app/Bookings/components/bookings-form/bookings-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings-form',
+  templateUrl: './bookings-form.component.html',
+  styleUrl: './bookings-form.component.scss'
+})
+export class BookingsFormComponent {
+
+}
---
+++ src/app/Bookings/components/bookings-list/bookings-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings-list',
+  templateUrl: './bookings-list.component.html',
+  styleUrl: './bookings-list.component.scss'
+})
+export class BookingsListComponent {
+
+}
