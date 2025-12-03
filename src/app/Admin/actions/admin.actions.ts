e1b1d5e8937bc59e83752ff4b914d426dfb7873c
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 13:53:24 +0100

    Created admin actions
    Included load makeupservices, success and failure. Create makeup service, success and failure, and delete makeup service, success and failure. Exported these actions

---
+++ src/app/Admin/actions/admin.actions.ts
@@ -0,0 +1,50 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { createAction, props } from '@ngrx/store';
+import { MakeupServiceDTO } from '../../Makeup_Services/models/makeup_service.dto';
+import { MakeupServiceCreateDTO } from '../services/admin.services';
+
+export const loadAdminMakeupServices = createAction(
+  '[Admin] Load MakeupServices'
+);
+
+export const loadAdminMakeupServicesSuccess = createAction(
+  '[Admin] Load MakeupServices Success',
+  props<{ makeup_services: MakeupServiceDTO[] }>()
+);
+
+export const loadAdminMakeupServicesFailure = createAction(
+  '[Admin] Load MakeupServices Failure',
+  props<{ error: HttpErrorResponse }>()
+);
+
+//crear servicio de maqullaje
+export const createAdminMakeupService = createAction(
+  '[Admin] Create Makeup Service',
+  props<{ payload: MakeupServiceCreateDTO }>()
+);
+
+export const createAdminMakeupServiceSuccess = createAction(
+  '[Admin] Create Makeup Service Success',
+  props<{ makeup_service: MakeupServiceDTO }>()
+);
+
+export const createAdminMakeupServiceFailure = createAction(
+  '[Admin] Create Makeup Service Failure',
+  props<{ error: HttpErrorResponse }>()
+);
+
+// Eliminar servicio de maquillaje
+export const deleteAdminMakeupService = createAction(
+  '[Admin] Delete Makeup Service',
+  props<{ id: number }>()
+);
+
+export const deleteAdminMakeupServiceSuccess = createAction(
+  '[Admin] Delete Makeup Service Success',
+  props<{ id: number }>()
+);
+
+export const deleteAdminMakeupServiceFailure = createAction(
+  '[Admin] Delete Makeup Service Failure',
+  props<{ error: HttpErrorResponse }>()
+);
