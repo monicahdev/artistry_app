a2c75935049055b22d6251f9e2e328aec1c642b4
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 23:05:43 +0100

    Added logic to bookings list form
    load bookings on init, and delete booking

--- src/app/Bookings/components/bookings-list/bookings-list.component.ts
+++ src/app/Bookings/components/bookings-list/bookings-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as BookingsActions from '../../actions/booking.actions';
+import { BookingDTO } from '../../models/booking.dto';
+import {
+  selectAllBookings,
+  selectBookingsLoading,
+} from '../../selectors/booking.selectors';
 
 @Component({
   selector: 'app-bookings-list',
   templateUrl: './bookings-list.component.html',
-  styleUrl: './bookings-list.component.scss'
+  styleUrls: ['./bookings-list.component.scss'],
 })
-export class BookingsListComponent {
+export class BookingsListComponent implements OnInit {
+  bookings$: Observable<BookingDTO[]>;
+  loading$: Observable<boolean>;
 
+  constructor(private store: Store) {
+    this.bookings$ = this.store.select(selectAllBookings);
+    this.loading$ = this.store.select(selectBookingsLoading);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(BookingsActions.loadMyBookings());
+  }
+
+  onDeleteBooking(id: number): void {
+    const confirmed = confirm('¿Quieres eliminar esta reserva?');
+    if (!confirmed) return;
+
+    this.store.dispatch(BookingsActions.deleteBooking({ id }));
+  }
 }
