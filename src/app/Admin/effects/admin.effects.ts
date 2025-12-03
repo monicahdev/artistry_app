f68e01c54faa8316365e1ade4562caaf0e3e8b4f
Monica Hernandez <monicahdev@gmail.com>
2025-12-03 14:20:32 +0100

    Included admin effects
    load MakeupServices, create MakeupService and deleteMakeupService

---
+++ src/app/Admin/effects/admin.effects.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, exhaustMap, map, mergeMap } from 'rxjs/operators';
+import * as MakeupServicesActions from '../../Makeup_Services/actions/makeup_services.actions';
+import * as AdminActions from '../actions/admin.actions';
+import { AdminService } from '../services/admin.services';
+
+@Injectable()
+export class AdminEffects {
+  constructor(private actions$: Actions, private adminService: AdminService) {}
+
+  loadMakeupServices$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AdminActions.loadAdminMakeupServices),
+      exhaustMap(() =>
+        this.adminService.getAllMakeupServices().pipe(
+          map((makeup_services) =>
+            AdminActions.loadAdminMakeupServicesSuccess({ makeup_services })
+          ),
+          catchError((error) =>
+            of(AdminActions.loadAdminMakeupServicesFailure({ error }))
+          )
+        )
+      )
+    )
+  );
+
+  createMakeupService$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AdminActions.createAdminMakeupService),
+      exhaustMap(({ payload }) =>
+        this.adminService.createMakeupService(payload).pipe(
+          mergeMap((makeup_service) => [
+            AdminActions.createAdminMakeupServiceSuccess({ makeup_service }),
+            MakeupServicesActions.loadMakeupServices(),
+          ]),
+          catchError((error) =>
+            of(AdminActions.createAdminMakeupServiceFailure({ error }))
+          )
+        )
+      )
+    )
+  );
+
+  deleteMakeupService$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AdminActions.deleteAdminMakeupService),
+      exhaustMap(({ id }) =>
+        this.adminService.deleteMakeupService(id).pipe(
+          mergeMap(() => [
+            AdminActions.deleteAdminMakeupServiceSuccess({ id }),
+            MakeupServicesActions.loadMakeupServices(),
+          ]),
+
+          catchError((error) =>
+            of(AdminActions.deleteAdminMakeupServiceFailure({ error }))
+          )
+        )
+      )
+    )
+  );
+}
