72c5fa186fdfdd9a43e0ae58cb7622dbe9f56e6f
Monica Hernandez <monicahdev@gmail.com>
2025-12-04 12:46:17 +0100

    Added getOnlineClasses service

---
+++ src/app/Online_Classes/services/online_classes.services.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { OnlineClassDTO } from '../models/online_classes.dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OnlineClassesService {
+  private readonly baseUrl = `${environment.apiUrl}/classes`;
+
+  constructor(private http: HttpClient) {}
+
+  getOnlineClasses(): Observable<OnlineClassDTO[]> {
+    return this.http.get<OnlineClassDTO[]>(this.baseUrl);
+  }
+}
