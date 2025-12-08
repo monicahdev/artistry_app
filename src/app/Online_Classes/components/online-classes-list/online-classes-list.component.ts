40b8e9a9dece64dd7972e0ea630bf5d5f8a5e711
Monica Hernandez <monicahdev@gmail.com>
2025-12-08 22:48:09 +0100

    Created online-classes logic
    load classes

--- src/app/Online_Classes/components/online-classes-list/online-classes-list.component.ts
+++ src/app/Online_Classes/components/online-classes-list/online-classes-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../../app.reducers';
+import * as OnlineClassesActions from '../../actions/online_classes.actions';
+import { OnlineClassDTO } from '../../models/online_classes.dto';
+import {
+  selectAllOnlineClasses,
+  selectOnlineClassesLoading,
+} from '../../selectors/online_classes.selectors';
 
 @Component({
   selector: 'app-online-classes-list',
   templateUrl: './online-classes-list.component.html',
-  styleUrl: './online-classes-list.component.scss'
+  styleUrls: ['./online-classes-list.component.scss'],
 })
-export class OnlineClassesListComponent {
+export class OnlineClassesListComponent implements OnInit {
+  classes$: Observable<OnlineClassDTO[]>;
+  loading$: Observable<boolean>;
 
+  constructor(private store: Store<AppState>) {
+    this.classes$ = this.store.select(selectAllOnlineClasses);
+    this.loading$ = this.store.select(selectOnlineClassesLoading);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(OnlineClassesActions.loadOnlineClasses());
+  }
 }
