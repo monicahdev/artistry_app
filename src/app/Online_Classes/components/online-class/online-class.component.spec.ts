60cc1e879345d871956869a5972fc74b58c2d553
Monica Hernandez <monicahdev@gmail.com>
2025-12-08 17:49:36 +0100

    Created online class component
    Will show class detail

---
+++ src/app/Online_Classes/components/online-class/online-class.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OnlineClassComponent } from './online-class.component';
+
+describe('OnlineClassComponent', () => {
+  let component: OnlineClassComponent;
+  let fixture: ComponentFixture<OnlineClassComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OnlineClassComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(OnlineClassComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Online_Classes/components/online-class/online-class.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-online-class',
+  templateUrl: './online-class.component.html',
+  styleUrl: './online-class.component.scss',
+})
+export class OnlineClassComponent {}
