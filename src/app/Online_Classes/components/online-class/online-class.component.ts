71739b918cf58d2b994d3236920e12f165a28aec
Monica Hernandez <monicahdev@gmail.com>
2025-12-08 23:29:15 +0100

    Included get inidivual class by id logic

--- src/app/Online_Classes/components/online-class/online-class.component.ts
+++ src/app/Online_Classes/components/online-class/online-class.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../../app.reducers';
+import * as OnlineClassesActions from '../../actions/online_classes.actions';
+import { OnlineClassDTO } from '../../models/online_classes.dto';
+import {
+  selectOnlineClassesLoading,
+  selectOnlineSelectedClass,
+} from '../../selectors/online_classes.selectors';
 @Component({
   selector: 'app-online-class',
   templateUrl: './online-class.component.html',
-  styleUrl: './online-class.component.scss',
+  styleUrls: ['./online-class.component.scss'],
 })
-export class OnlineClassComponent {}
+export class OnlineClassComponent implements OnInit {
+  onlineClass$: Observable<OnlineClassDTO | null>;
+  loading$: Observable<boolean>;
+
+  constructor(private route: ActivatedRoute, private store: Store<AppState>) {
+    this.onlineClass$ = this.store.select(selectOnlineSelectedClass);
+    this.loading$ = this.store.select(selectOnlineClassesLoading);
+  }
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      const id = +idParam;
+      this.store.dispatch(OnlineClassesActions.loadOnlineClassDetail({ id }));
+    }
+  }
+}
