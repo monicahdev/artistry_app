8dc0eb93f6c7a813a609e35b4acd4c7456a19be9
Monica Hernandez <monicahdev@gmail.com>
2025-12-08 18:37:21 +0100

    Imported OnlineClasses effects

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -2,6 +2,7 @@ import { AdminEffects } from './Admin/effects/admin.effects';
 import { AuthEffects } from './Auth/effects/auth.effects';
 import { BookingsEffects } from './Bookings/effects/booking.effects';
 import { MakeupServicesEffects } from './Makeup_Services/effects/makeup_services.effects';
+import { OnlineClassesEffects } from './Online_Classes/effects/online_classes.effects';
 import { UserEffects } from './User/effects/user.effects';
 export const appEffects = [
   AuthEffects,
@@ -9,4 +10,5 @@ export const appEffects = [
   MakeupServicesEffects,
   AdminEffects,
   BookingsEffects,
+  OnlineClassesEffects,
 ];
