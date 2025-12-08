acf4f6ec8fb755a56958d9c42f579ba5f03a94e8
Monica Hernandez <monicahdev@gmail.com>
2025-12-08 16:36:19 +0100

    Added online class detail actions

--- src/app/Online_Classes/actions/online_classes.actions.ts
+++ src/app/Online_Classes/actions/online_classes.actions.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { createAction, props } from '@ngrx/store';
 import { OnlineClassDTO } from '../models/online_classes.dto';
 
+//al classes
 export const loadOnlineClasses = createAction('[Classes] Load OnlineClasses');
 
 export const loadOnlineClassesSuccess = createAction(
@@ -11,5 +12,21 @@ export const loadOnlineClassesSuccess = createAction(
 
 export const loadOnlineClassesFailure = createAction(
   '[Classes] Load OnlineClasses Failure',
+  props<{ error: HttpErrorResponse }>()
+);
+
+//class detail
+export const loadOnlineClassDetail = createAction(
+  '[Classes] Load OnlineClass Detail',
+  props<{ id: number }>()
+);
+
+export const loadOnlineClassDetailSuccess = createAction(
+  '[Classes] Load OnlineClass Detail Success',
+  props<{ onlineClass: OnlineClassDTO }>()
+);
+
+export const loadOnlineClassDetailFailure = createAction(
+  '[Classes] Load OnlineClass Detail Failure',
   props<{ error: HttpErrorResponse }>()
 );
