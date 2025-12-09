a9de9d4f3c23665cfd2a83a421ef59c4d7e53120
Monica Hernandez <monicahdev@gmail.com>
2025-12-09 13:23:44 +0100

    Added individual makeup service logic

--- src/app/Makeup_Services/components/makeup-service/makeup-service.component.ts
+++ src/app/Makeup_Services/components/makeup-service/makeup-service.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../../app.reducers';
+import * as MakeupServicesActions from '../../actions/makeup_services.actions';
+import { MakeupServiceDTO } from '../../models/makeup_service.dto';
+import {
+  selectMakeupServicesLoading,
+  selectSelectedMakeupService,
+} from '../../selectors/makeup_service.selectors';
 @Component({
   selector: 'app-makeup-service',
   templateUrl: './makeup-service.component.html',
-  styleUrl: './makeup-service.component.scss'
+  styleUrl: './makeup-service.component.scss',
 })
 export class MakeupServiceComponent {
+  makeupService$: Observable<MakeupServiceDTO | null>;
+  loading$: Observable<boolean>;
 
+  constructor(
+    private route: ActivatedRoute,
+    public router: Router,
+    private store: Store<AppState>
+  ) {
+    this.makeupService$ = this.store.select(selectSelectedMakeupService);
+    this.loading$ = this.store.select(selectMakeupServicesLoading);
+  }
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      const id = +idParam;
+      this.store.dispatch(MakeupServicesActions.loadMakeupServiceById({ id }));
+    }
+  }
+
+  onBook(serviceId: number): void {
+    this.router.navigate(['/bookings/new'], {
+      queryParams: { serviceId },
+    });
+  }
 }
