d603a7b0ba7fe90fa75a8aaa10217355104b065b
Monica Hernandez <monicahdev@gmail.com>
2025-12-09 15:55:24 +0100

    Added updateBooking service

--- src/app/Bookings/services/booking.services.ts
+++ src/app/Bookings/services/booking.services.ts
@@ -2,7 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { BookingCreateDTO, BookingDTO } from '../models/booking.dto';
+import {
+  BookingCreateDTO,
+  BookingDTO,
+  BookingUpdateDTO,
+} from '../models/booking.dto';
 
 @Injectable({
   providedIn: 'root',
@@ -22,5 +26,9 @@ export class BookingsService {
 
   deleteBooking(id: number): Observable<void> {
     return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+
+  updateBooking(id: number, update: BookingUpdateDTO): Observable<BookingDTO> {
+    return this.http.patch<BookingDTO>(`${this.baseUrl}/${id}`, update);
   }
 }
