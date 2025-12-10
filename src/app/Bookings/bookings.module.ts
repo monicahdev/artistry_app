ea7c7561cbd96cd7718d9725b68f952e8aab0760
Monica Hernandez <monicahdev@gmail.com>
2025-12-10 21:13:49 +0100

    imported Router Moduel

--- src/app/Bookings/bookings.module.ts
+++ src/app/Bookings/bookings.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BookingsFormComponent } from './components/bookings-form/bookings-form.component';
 import { BookingsListComponent } from './components/bookings-list/bookings-list.component';
-
 @NgModule({
   declarations: [BookingsListComponent, BookingsFormComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [BookingsListComponent, BookingsFormComponent],
 })
 export class BookingModule {}
