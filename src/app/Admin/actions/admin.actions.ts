01fbd8d2e91ce659bc42e96b6c4c91f203c4c66e
Monica Hernandez <monicahdev@gmail.com>
2025-12-12 19:23:35 +0100

    Added admin actions
    action, success and failure for loading all online classes, creating, updating, and deleting an online class, and granting a user access to an online class

--- src/app/Admin/actions/admin.actions.ts
+++ src/app/Admin/actions/admin.actions.ts
@@ -1,6 +1,11 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { createAction, props } from '@ngrx/store';
 import { MakeupServiceDTO } from '../../Makeup_Services/models/makeup_service.dto';
+import {
+  OnlineClassCreateDTO,
+  OnlineClassDTO,
+  OnlineClassUpdateDTO,
+} from '../../Online_Classes/models/online_classes.dto';
 import { UserDTO } from '../../User/models/user.dto';
 import { MakeupServiceCreateDTO } from '../services/admin.services';
 
@@ -77,4 +82,82 @@ export const loadAllUsersSuccess = createAction(
 export const loadAllUsersFailure = createAction(
   '[Admin] Load All Users Failure',
   props<{ error: any }>()
+);
+
+//listar clases
+export const loadAdminOnlineClasses = createAction(
+  '[Admin] Load Online Classes'
+);
+
+export const loadAdminOnlineClassesSuccess = createAction(
+  '[Admin] Load Online Classes Success',
+  props<{ classes: OnlineClassDTO[] }>()
+);
+
+export const loadAdminOnlineClassesFailure = createAction(
+  '[Admin] Load Online Classes Failure',
+  props<{ error: HttpErrorResponse }>()
+);
+
+//crfear clase en línea
+export const createAdminOnlineClass = createAction(
+  '[Admin] Create Online Class',
+  props<{ payload: OnlineClassCreateDTO }>()
+);
+
+export const createAdminOnlineClassSuccess = createAction(
+  '[Admin] Create Online Class Success',
+  props<{ online_class: OnlineClassDTO }>()
+);
+
+export const createAdminOnlineClassFailure = createAction(
+  '[Admin] Create Online Class Failure',
+  props<{ error: HttpErrorResponse }>()
+);
+
+//actualizar clase en línea
+export const updateAdminOnlineClass = createAction(
+  '[Admin] Update Online Class',
+  props<{ id: number; payload: OnlineClassUpdateDTO }>()
+);
+
+export const updateAdminOnlineClassSuccess = createAction(
+  '[Admin] Update Online Class Success',
+  props<{ online_class: OnlineClassDTO }>()
+);
+
+export const updateAdminOnlineClassFailure = createAction(
+  '[Admin] Update Online Class Failure',
+  props<{ error: HttpErrorResponse }>()
+);
+
+//eliminar claser
+export const deleteAdminOnlineClass = createAction(
+  '[Admin] Delete Online Class',
+  props<{ id: number }>()
+);
+
+export const deleteAdminOnlineClassSuccess = createAction(
+  '[Admin] Delete Online Class Success',
+  props<{ id: number }>()
+);
+
+export const deleteAdminOnlineClassFailure = createAction(
+  '[Admin] Delete Online Class Failure',
+  props<{ error: HttpErrorResponse }>()
+);
+
+//dar acceso a clase a un usuario
+export const grantOnlineClassAccess = createAction(
+  '[Admin] Grant Online Class Access',
+  props<{ classId: number; userId: number }>()
+);
+
+export const grantOnlineClassAccessSuccess = createAction(
+  '[Admin] Grant Online Class Access Success'
+);
+
+export const grantOnlineClassAccessFailure = createAction(
+  '[Admin] Grant Online Class Access Failure',
+  props<{ error: HttpErrorResponse }>()
 );
