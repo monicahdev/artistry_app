6692a7b6b6984b7685441be330801c35acd116e7
Monica Hernandez <monicahdev@gmail.com>
2025-12-12 19:56:09 +0100

    Added admin online classes services
    Admin can list all online classes, create, update, and delete a class, as well as grant a user acccess to a specific class

--- src/app/Admin/services/admin.services.ts
+++ src/app/Admin/services/admin.services.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { MakeupServiceDTO } from '../../Makeup_Services/models/makeup_service.dto';
+import { OnlineClassDTO } from '../../Online_Classes/models/online_classes.dto';
 import { UserDTO } from '../../User/models/user.dto';
 export interface MakeupServiceCreateDTO {
   //makeup_service
@@ -20,12 +21,26 @@ export interface MakeupServiceUpdateDTO {
   price_from?: number;
   duration?: number;
 }
+
+export interface OnlineClassCreateDTO {
+  name: string;
+  description: string;
+  url: string;
+}
+
+export interface OnlineClassUpdateDTO {
+  name?: string;
+  description?: string;
+  url?: string;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class AdminService {
   private readonly baseUrl = `${environment.apiUrl}/admin/services`;
   private readonly baseUsersUrl = `${environment.apiUrl}/users`;
+  private readonly baseClassesUrl = `${environment.apiUrl}/admin/classes`;
+
   constructor(private http: HttpClient) {}
 
   getAllMakeupServices(): Observable<MakeupServiceDTO[]> {
@@ -51,5 +66,32 @@ export class AdminService {
 
   getAllUsers(): Observable<UserDTO[]> {
     return this.http.get<UserDTO[]>(`${this.baseUsersUrl}/all-users`);
+  }
+
+  getAllOnlineClasses(): Observable<OnlineClassDTO[]> {
+    return this.http.get<OnlineClassDTO[]>(this.baseClassesUrl);
+  }
+
+  createOnlineClass(payload: OnlineClassCreateDTO): Observable<OnlineClassDTO> {
+    return this.http.post<OnlineClassDTO>(this.baseClassesUrl, payload);
+  }
+
+  updateOnlineClass(
+    id: number,
+    payload: OnlineClassUpdateDTO
+  ): Observable<OnlineClassDTO> {
+    return this.http.patch<OnlineClassDTO>(
+      `${this.baseClassesUrl}/${id}`,
+      payload
+    );
+  }
+
+  deleteOnlineClass(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseClassesUrl}/${id}`);
+  }
+
+  grantOnlineClassAccess(classId: number, userId: number): Observable<void> {
+    const url = `${this.baseClassesUrl}/${classId}/grant-access/${userId}`;
+    return this.http.post<void>(url, {});
   }
 }
