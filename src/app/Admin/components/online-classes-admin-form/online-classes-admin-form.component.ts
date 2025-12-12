66bc6262d41b86b70f3d4a60737efa7de1fd9c51
Monica Hernandez <monicahdev@gmail.com>
2025-12-12 23:12:44 +0100

    Added admin online classes form logic
    ngOnInit, onSubmit and onDelete methods

--- src/app/Admin/components/online-classes-admin-form/online-classes-admin-form.component.ts
+++ src/app/Admin/components/online-classes-admin-form/online-classes-admin-form.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../app.reducers';
+import * as AdminActions from '../../actions/admin.actions';
+import {
+  OnlineClassCreateDTO,
+  OnlineClassUpdateDTO,
+} from '../../services/admin.services';
 
 @Component({
   selector: 'app-online-classes-admin-form',
   templateUrl: './online-classes-admin-form.component.html',
-  styleUrl: './online-classes-admin-form.component.scss'
+  styleUrl: './online-classes-admin-form.component.scss',
 })
-export class OnlineClassesAdminFormComponent {
+export class OnlineClassesAdminFormComponent implements OnInit {
+  classForm: FormGroup;
+  name = new FormControl('', Validators.required);
+  description = new FormControl('', Validators.required);
+  url = new FormControl('', [Validators.required]);
 
+  isEditMode = false;
+  classId: number | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppState>,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.classForm = this.fb.group({
+      name: this.name,
+      description: this.description,
+      url: this.url,
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      const idParam = params.get('id');
+      const nameParam = params.get('name');
+      const descriptionParam = params.get('description');
+      const urlParam = params.get('url');
+
+      if (idParam) {
+        this.isEditMode = true;
+        this.classId = +idParam;
+      }
+
+      if (nameParam) this.name.setValue(nameParam);
+      if (descriptionParam) this.description.setValue(descriptionParam);
+      if (urlParam) this.url.setValue(urlParam);
+    });
+  }
+
+  onSubmit(): void {
+    if (this.classForm.invalid) {
+      this.classForm.markAllAsTouched();
+      return;
+    }
+
+    const payload: OnlineClassCreateDTO = {
+      name: this.name.value!,
+      description: this.description.value!,
+      url: this.url.value!,
+    };
+
+    if (this.isEditMode && this.classId !== null) {
+      const updatePayload: OnlineClassUpdateDTO = {
+        ...payload,
+      };
+
+      this.store.dispatch(
+        AdminActions.updateAdminOnlineClass({
+          id: this.classId,
+          payload: updatePayload,
+        })
+      );
+    } else {
+      this.store.dispatch(AdminActions.createAdminOnlineClass({ payload }));
+    }
+    this.router.navigate(['/admin/classes']);
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/admin/classes']);
+  }
 }
