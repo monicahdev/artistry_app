dd801d77f24ed90dba1d27a8653def00aabb659b
Monica Hernandez <monicahdev@gmail.com>
2025-12-12 22:19:12 +0100

    Added admin reducers
    load classes, success and failure for loading all online classes, creating, updating, and deleting an online class, and granting a user access to an online class

--- src/app/Admin/reducers/admin.reducers.ts
+++ src/app/Admin/reducers/admin.reducers.ts
@@ -1,8 +1,8 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { MakeupServiceDTO } from '../../Makeup_Services/models/makeup_service.dto';
+import { OnlineClassDTO } from '../../Online_Classes/models/online_classes.dto';
 import { UserDTO } from '../../User/models/user.dto';
 import * as AdminActions from '../actions/admin.actions';
-
 export interface AdminState {
   users: UserDTO[];
   loadingUsers: boolean;
@@ -13,6 +13,13 @@ export interface AdminState {
   loading: boolean;
   loaded: boolean;
   error: any;
+
+  online_classes: OnlineClassDTO[];
+  loadingClasses: boolean;
+  classesLoaded: boolean;
+  classesError: any;
+  grantingAccess: boolean;
+  grantAccessError: any;
 }
 
 export const initialState: AdminState = {
@@ -25,6 +32,13 @@ export const initialState: AdminState = {
   loading: false,
   loaded: false,
   error: null,
+
+  online_classes: [],
+  loadingClasses: false,
+  classesLoaded: false,
+  classesError: null,
+  grantingAccess: false,
+  grantAccessError: null,
 };
 
 const _adminReducer = createReducer(
@@ -135,6 +149,97 @@ const _adminReducer = createReducer(
     ...state,
     loadingUsers: false,
     usersError: error,
+  })),
+
+  // cargar todas las clases
+  on(AdminActions.loadAdminOnlineClasses, (state) => ({
+    ...state,
+    loadingClasses: true,
+    classesLoaded: false,
+    classesError: null,
+  })),
+  on(AdminActions.loadAdminOnlineClassesSuccess, (state, { classes }) => ({
+    ...state,
+    online_classes: classes,
+    loadingClasses: false,
+    classesLoaded: true,
+    classesError: null,
+  })),
+  on(AdminActions.loadAdminOnlineClassesFailure, (state, { error }) => ({
+    ...state,
+    loadingClasses: false,
+    classesLoaded: false,
+    classesError: error,
+  })),
+
+  //crear clase
+  on(AdminActions.createAdminOnlineClass, (state) => ({
+    ...state,
+    loadingClasses: true,
+  })),
+  on(AdminActions.createAdminOnlineClassSuccess, (state, { online_class }) => ({
+    ...state,
+    online_classes: [...state.online_classes, online_class],
+    loadingClasses: false,
+    classesLoaded: true,
+  })),
+  on(AdminActions.createAdminOnlineClassFailure, (state, { error }) => ({
+    ...state,
+    loadingClasses: false,
+    classesError: error,
+  })),
+
+  //actualizar clase
+  on(AdminActions.updateAdminOnlineClass, (state) => ({
+    ...state,
+    loadingClasses: true,
+  })),
+  on(AdminActions.updateAdminOnlineClassSuccess, (state, { online_class }) => ({
+    ...state,
+    online_classes: state.online_classes.map((c) =>
+      c.id === online_class.id ? online_class : c
+    ),
+    loadingClasses: false,
+    classesLoaded: true,
+    classesError: null,
+  })),
+  on(AdminActions.updateAdminOnlineClassFailure, (state, { error }) => ({
+    ...state,
+    loadingClasses: false,
+    classesError: error,
+  })),
+
+  //eliminar clase
+  on(AdminActions.deleteAdminOnlineClass, (state) => ({
+    ...state,
+    loadingClasses: true,
+  })),
+  on(AdminActions.deleteAdminOnlineClassSuccess, (state, { id }) => ({
+    ...state,
+    online_classes: state.online_classes.filter((c) => c.id !== id),
+    loadingClasses: false,
+    classesLoaded: true,
+  })),
+  on(AdminActions.deleteAdminOnlineClassFailure, (state, { error }) => ({
+    ...state,
+    loadingClasses: false,
+    classesError: error,
+  })),
+
+  //dar acceso a clase a un usuario
+  on(AdminActions.grantOnlineClassAccess, (state) => ({
+    ...state,
+    loadingClasses: true,
+    classesError: null,
+  })),
+  on(AdminActions.grantOnlineClassAccessSuccess, (state) => ({
+    ...state,
+    loadingClasses: false,
+  })),
+  on(AdminActions.grantOnlineClassAccessFailure, (state, { error }) => ({
+    ...state,
+    loadingClasses: false,
+    classesError: error,
   }))
 );
 export function adminReducer(
