31b74da7a2bce6ca174f10d0f4f6afb90679bfee
Mónica Hernández <monicahdev@gmail.com>
2025-12-17 15:09:55 +0100

    Delete unnecesary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,16 +15,15 @@ import { BookingModule } from './Bookings/bookings.module';
 import { MakeupServiceComponent } from './Makeup_Services/components/makeup-service/makeup-service.component';
 import { MakeupServicesListComponent } from './Makeup_Services/components/makeup_services-list/makeup_services-list.component';
 import { OnlineClassComponent } from './Online_Classes/components/online-class/online-class.component';
-import { OnlineClassesFormComponent } from './Online_Classes/components/online-classes-form/online-classes-form.component';
 import { OnlineClassesListComponent } from './Online_Classes/components/online-classes-list/online-classes-list.component';
 import { AboutMeComponent } from './Public/about-me/about-me.component';
 import { HomeComponent } from './Public/home/home.component';
 import { PortfolioComponent } from './Public/portfolio/portfolio.component';
 import { FooterComponent } from './Shared/Components/footer/footer.component';
 import { HeaderComponent } from './Shared/Components/header/header.component';
+import { ToastComponent } from './Shared/Components/toast/toast.component';
 import { AuthInterceptor } from './Shared/Services/auth-interceptor.service';
 import { ProfileComponent } from './User/components/profile/profile.component';
-import { ToastComponent } from './Shared/Components/toast/toast.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +36,6 @@ import { ToastComponent } from './Shared/Components/toast/toast.component';
     ProfileComponent,
     MakeupServicesListComponent,
     OnlineClassesListComponent,
-    OnlineClassesFormComponent,
     OnlineClassComponent,
     MakeupServiceComponent,
     ToastComponent,
