b71e6abaeb83e3d5f522779866b9deca67bf6979
Mónica Hernández <monicahdev@gmail.com>
2025-12-17 16:21:07 +0100

    Added toogle burger menu in ts, and added elements to header html

--- src/app/Shared/Components/header/header.component.ts
+++ src/app/Shared/Components/header/header.component.ts
@@ -16,13 +16,24 @@ export class HeaderComponent {
   isAuthenticated$: Observable<boolean>;
   currentUser$: Observable<UserDTO | null>;
 
+  isMenuOpen = false;
+
   constructor(private store: Store<AppState>) {
     this.isAuthenticated$ = this.store.select(selectIsAuthenticated);
     this.currentUser$ = this.store.select(selectCurrentUser);
   }
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
   logout(): void {
     this.store.dispatch(AuthActions.logout());
     localStorage.removeItem('access_token');
+    this.closeMenu();
   }
 }
